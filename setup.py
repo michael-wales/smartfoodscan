95118cd0c3cd9ff37a2a6172bf499308d84e5694
Michael Wales <104277040+michael-wales@users.noreply.github.com>
2025-02-18 17:03:01 -0500

    Playing with the data

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="smartfoodscan",
+    version="0.1.0",
+    description="",
+    long_description=open("README.md").read(),
+    author="",
+    author_email="",
+    url="",
+    packages=find_packages(where="src"),
+    license="",
+    package_dir={"": "src"},
+
+    install_requires = open("requirements.txt").read().splitlines()
+)
